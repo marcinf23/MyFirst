735282f24bd711ff82b2e490c221092caf431d50
Marcin Frydrych <marcinf23@gmail.com>
2023-04-26 02:03:02 +0200

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Marcin Frydrych on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
